f18fb0e2d285c2b577bc80e4be8fc411cfa25925
isa öztürk <isaozturk@gmail.com>
2019-06-17 00:06:14 +0300

    city service unit test

---
+++ src/test/java/io/sisa/CityServiceTest.java
@@ -0,0 +1,38 @@
+package io.sisa;
+
+import io.sisa.core.model.domain.City;
+import io.sisa.core.model.service.CityService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by isaozturk on 17.06.2019
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CityServiceTest {
+
+    @Autowired
+    private CityService cityService;
+
+    @Test
+    public void fetchAllCitiesTest() {
+
+        List<City> cities = cityService.fetchAllCities();
+        assertThat(cities).isNotNull();
+    }
+
+    @Test
+    public void findCityyByIdTest() {
+
+        City city = cityService.findById(1L);
+        assertThat(city).isNotNull();
+    }
+}
